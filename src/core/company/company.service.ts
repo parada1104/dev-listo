6e94af88eb9ea7415da9cf97d535f6aa419c08f6
robert parada <parada11.04@gmail.com>
2021-08-05 11:23:19 -0400

    company and employee services initial setup

--- src/core/company/company.service.ts
+++ src/core/company/company.service.ts
@@ -1,4 +1,29 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable } from "@nestjs/common";
+import { InjectModel } from "@nestjs/mongoose";
+import { Company, CompanyDocument } from "./domain/company.schema";
+import { Model } from "mongoose";
 
 @Injectable()
-export class CompanyService {}
+export class CompanyService {
+  constructor(
+    @InjectModel(Company.name)
+    private readonly _companyModel: Model<CompanyDocument>,
+  ) {}
+
+  async findMany(): Promise<Company[]> {
+    return this._companyModel.find().lean();
+  }
+
+  async findById(id: string): Promise<Company> {
+    return this._companyModel.findById(id).lean();
+  }
+
+  async createCompany(company: Company): Promise<Company> {
+    return this._companyModel.create(company);
+  }
+
+  async deleteCompany(id: string) {
+    const result = await this._companyModel.deleteOne({ id: id });
+    return result.deletedCount > 0;
+  }
+}
--- src/core/employee/employee.service.ts
+++ src/core/employee/employee.service.ts
@@ -1,4 +1,24 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable } from "@nestjs/common";
+import { InjectModel } from "@nestjs/mongoose";
+import { Employee, EmployeeDocument } from "./domain/employee.schema";
+import { Model } from "mongoose";
 
 @Injectable()
-export class EmployeeService {}
+export class EmployeeService {
+  constructor(
+    @InjectModel(Employee.name)
+    private readonly _employeeModel: Model<EmployeeDocument>,
+  ) {}
+
+  async findAll(): Promise<Employee> {
+    return this._employeeModel.find().lean();
+  }
+
+  async findById(id: string): Promise<Employee> {
+    return this._employeeModel.findById(id).lean();
+  }
+
+  async findByCompanyId(companyId: string): Promise<Employee[]> {
+    return this._employeeModel.find({ where: { company: companyId } });
+  }
+}
