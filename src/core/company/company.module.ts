52ed5c15a69c330dead1f767fce239ebdf4631b1
robert parada <parada11.04@gmail.com>
2021-08-05 10:08:41 -0400

    all set with mongo to start with services

--- src/core/company/company.module.ts
+++ src/core/company/company.module.ts
@@ -1,8 +1,18 @@
-import { Module } from '@nestjs/common';
-import { CompanyService } from './company.service';
-import { CompanyResolver } from './resolvers/company.resolver';
+import { Module } from "@nestjs/common";
+import { CompanyService } from "./company.service";
+import { CompanyResolver } from "./resolvers/company.resolver";
+import { MongooseModule } from "@nestjs/mongoose";
+import { Company, CompanySchema } from "./domain/company.schema";
+import { Employee, EmployeeSchema } from "../employee/domain/employee.schema";
+import { EmployeeService } from "../employee/employee.service";
 
 @Module({
-  providers: [CompanyService, CompanyResolver],
+  imports: [
+    MongooseModule.forFeature([
+      { name: Company.name, schema: CompanySchema },
+      { name: Employee.name, schema: EmployeeSchema },
+    ]),
+  ],
+  providers: [CompanyService, CompanyResolver, EmployeeService],
 })
 export class CompanyModule {}
--- src/core/employee/employee.module.ts
+++ src/core/employee/employee.module.ts
@@ -1,8 +1,18 @@
-import { Module } from '@nestjs/common';
-import { EmployeeService } from './employee.service';
-import { EmployeeResolver } from './resolvers/employee.resolver';
+import { Module } from "@nestjs/common";
+import { EmployeeService } from "./employee.service";
+import { EmployeeResolver } from "./resolvers/employee.resolver";
+import { MongooseModule } from "@nestjs/mongoose";
+import { Company, CompanySchema } from "../company/domain/company.schema";
+import { Employee, EmployeeSchema } from "./domain/employee.schema";
+import { CompanyService } from "../company/company.service";
 
 @Module({
-  providers: [EmployeeService, EmployeeResolver],
+  imports: [
+    MongooseModule.forFeature([
+      { name: Company.name, schema: CompanySchema },
+      { name: Employee.name, schema: EmployeeSchema },
+    ]),
+  ],
+  providers: [EmployeeService, EmployeeResolver, CompanyService],
 })
 export class EmployeeModule {}
