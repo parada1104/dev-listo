9ded5581644da634bd1169d342e6c29a20419ccb
robert parada <parada11.04@gmail.com>
2021-08-05 18:22:00 -0400

    config refactor

--- src/config/config.service.ts
+++ src/config/config.service.ts
@@ -1,6 +1,6 @@
-import * as fs from 'fs';
-import { parse } from 'dotenv';
-import { Configuration } from './config.keys';
+import * as fs from "fs";
+import { parse } from "dotenv";
+import { Configuration } from "./config.keys";
 
 export interface IEnvConfig {
   [key: string]: string;
@@ -10,20 +10,19 @@ export class ConfigService {
   private readonly _envConfig: IEnvConfig;
 
   constructor() {
-    const isDev: boolean = process.env.NODE_ENV !== 'production';
+    const isDev: boolean = process.env.NODE_ENV !== "production";
     if (isDev) {
-      const envFilePath: string = __dirname + '/../../.env';
-      console.log(envFilePath);
+      const envFilePath: string = __dirname + "/../../.env";
       const existsPath: boolean = fs.existsSync(envFilePath);
       if (!existsPath) {
-        console.log('.Env is not present');
+        console.log(".Env is not present");
         process.exit(0);
       }
       this._envConfig = parse(fs.readFileSync(envFilePath));
-      console.log(this._envConfig);
     } else {
       this._envConfig = {
-        PORT: process.env.PORT,
+        PORT: process.env[Configuration.PORT],
+        ATLAS_URL: process.env[Configuration.ATLAS_URL],
       };
     }
   }
